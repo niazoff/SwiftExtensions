819b5e3d400af9ccc7e766e0321fd8eb9244861c
Natanel Niazoff <nniazoff@icloud.com>
2020-07-21 22:23:33 +0300

    Add optional

---
+++ Sources/SwiftExtensions/Optional.swift
@@ -0,0 +1,8 @@
+public extension Optional {
+  static func ?? (lhs: Wrapped?, rhs: @autoclosure () -> Never) -> Wrapped {
+    switch lhs {
+    case .none: rhs()
+    case .some(let unwrapped): return unwrapped
+    }
+  }
+}
