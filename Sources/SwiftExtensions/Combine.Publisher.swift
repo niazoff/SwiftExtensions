3dd4e5a4de01e031764a214fcb7b4f7d5c6d4b20
Natanel Niazoff <nniazoff@icloud.com>
2020-07-27 18:29:39 +0300

    Add Publisher

---
+++ Sources/SwiftExtensions/Combine.Publisher.swift
@@ -0,0 +1,8 @@
+import Combine
+
+@available(iOS 13, macOS 10.15, macCatalyst 13, tvOS 13, watchOS 6, *)
+public extension Publisher where Failure == Never {
+  func assign<Root: AnyObject>(to keyPath: ReferenceWritableKeyPath<Root, Output>, onWeak object: Root) -> AnyCancellable {
+    sink { [weak object] in object?[keyPath: keyPath] = $0 }
+  }
+}
