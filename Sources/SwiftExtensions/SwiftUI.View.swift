a365636da01e0bb17fe9b4f7364c4b5a2cbdb1ce
Natanel Niazoff <nniazoff@icloud.com>
2021-09-19 00:20:41 -0400

    Add if view modifier

--- Sources/SwiftExtensions/SwiftUI.View.swift
+++ Sources/SwiftExtensions/SwiftUI.View.swift
@@ -2,4 +2,12 @@ import SwiftUI
 
 public extension View {
   func eraseToAnyView() -> AnyView { AnyView(self) }
+  
+  @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+    if condition {
+      transform(self)
+    } else {
+      self
+    }
+  }
 }
